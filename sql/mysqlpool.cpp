d864c47faf13922f1533f34c929b040f70284177
TonyBeen <1772747354@qq.com>
2021-11-19 17:24:15 +0800

    modify mysqlpool

--- sql/mysqlpool.cpp
+++ sql/mysqlpool.cpp
@@ -5,11 +5,5 @@
     > Created Time: Wed 10 Nov 2021 09:47:56 AM CST
  ************************************************************************/
 
-#include <iostream>
-using namespace std;
+#include "mysqlpool.h"
 
-int main(int argc, char **argv)
-{
-
-    return 0;
-}
--- sql/mysqlpool.h
+++ sql/mysqlpool.h
@@ -5,11 +5,26 @@
     > Created Time: Wed 10 Nov 2021 09:47:53 AM CST
  ************************************************************************/
 
-#include <iostream>
-using namespace std;
+#ifndef __MYSQL_POOL_H__
+#define __MYSQL_POOL_H__
 
-int main(int argc, char **argv)
+#include <utils/string8.h>
+#include <stdint.h>
+#include <vector>
+
+namespace Jarvis {
+class MysqlPool
 {
+public:
+    MysqlPool(uint32_t sqlHandleNum, const String8 &userName = "mysql",
+              const String8 &passWord = "123456", const String8 &IP = "127.0.0.1", const uint16_t &port = 3306);
+    ~MysqlPool();
 
-    return 0;
-}
+
+private:
+
+};
+
+} // namespace Jarvis
+
+#endif // __MYSQL_POOL_H__
